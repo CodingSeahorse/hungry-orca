fee9e584bc99bc7af5e2fd633c118bbd47d784df
Sarah Lena De Matos Silva Knell <69032664+BeSunrise@users.noreply.github.com>
2021-08-31 21:59:08 +0200

    starting point

---
+++ src/test/java/com/codingseahorse/hungryorca/HungryOrcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingseahorse.hungryorca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungryOrcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
